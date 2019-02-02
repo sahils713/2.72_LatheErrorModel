d7b63c63987939ca503df36ce0e67f099a63ff88
Sahil Shah <47264096+sahils713@users.noreply.github.com>
2019-02-01 23:39:47 -0500

    Revert "Delete PrecisionAndAccuracy.m"
    This reverts commit b9d5e991261d77fa44f8e2bbc23cfc7b6841c55f.

---
+++ superceeded/March8/PrecisionAndAccuracy.m
@@ -0,0 +1,35 @@
+LatheSpecs;
+
+partDia = 10:1:13; %[mm]
+partLength = 20:5:100; %[mm]
+
+errorX = size(length(partDia),length(partLength)); % Error Terms
+
+Fn = 10;
+Ft = 100;
+Fa = 10;
+
+%% CALCULATE ERROR
+
+for i = 1:length(partDia)
+    lathe.D1 = partDia(i);
+    
+    for j = 1:length(partLength);
+        
+        lathe.L1 = partLength(j);
+        [delP, ~, ~, ~, ~] = HTM_Error_Estimator(Fn, Ft, Fa, lathe, 0);
+        
+        errorX(i,j) = delP(2);
+        
+    end
+    fprintf('\nRun %3.0f of %4.0f',i*j,length(partDia)*length(partLength));
+end
+
+%% PLOT ERROR
+figure()
+hold on
+surf(partLength,partDia,errorX.*1000);
+xlabel('Part Length [mm]');
+ylabel('Part Diameter [mm]');
+zlabel('Modeled Error [um]');
+hold off
